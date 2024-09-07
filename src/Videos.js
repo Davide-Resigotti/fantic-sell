8b887394571662aafc7397b8cbf9b8642edb123e
Davide-Resigotti <idavide.resi@gmail.com>
2024-09-07 12:00:53 +0200

    metadata in videos

--- src/Videos.js
+++ src/Videos.js
@@ -10,61 +10,54 @@ function Videos() {
     <div className="projectsPage">
 
       <div className="projectTitle">
-        <Link to="/">
-          <FaHome className='iconHome' />
-        </Link>
-        <h1 className='foto'>VIDEO</h1>
+      <Link to="/">
+        <FaHome className='iconHome' />
+      </Link>
+      <h1 className='foto'>VIDEO</h1>
       </div>
       <div className="projectRectangles" >
-        <video  alt="accensione" controls audio  className="rectangle" >
-          <source src="./start.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
+      <video alt="accensione" controls audio className="rectangle" preload="metadata">
+        <source src="./start.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
 
-        <video controls audio  className="rectangle" >
-          <source src="./left-arrow.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
+      <video controls audio className="rectangle" preload="metadata">
+        <source src="./left-arrow.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
 
-        <video controls audio  className="rectangle" >
-          <source src="./right-arrow.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
+      <video controls audio className="rectangle" preload="metadata">
+        <source src="./right-arrow.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
 
-        <video controls audio  className="rectangle" >
-          <source src="./high-beam-headlights.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
+      <video controls audio className="rectangle" preload="metadata">
+        <source src="./high-beam-headlights.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
 
-        <video controls audio  className="rectangle" >
-          <source src="./stop-light.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
+      <video controls audio className="rectangle" preload="metadata">
+        <source src="./stop-light.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
 
-        <video controls audio  className="rectangle" >
-          <source src="./degassing.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
+      <video controls audio className="rectangle" preload="metadata">
+        <source src="./degassing.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
 
-        <video controls audio  className="rectangle" >
-          <source src="./engine-off.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
+      <video controls audio className="rectangle" preload="metadata">
+        <source src="./engine-off.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
 
-        <video controls audio  className="rectangle" >
-          <source src="./plate.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-
-
-
-
-
-
-
+      <video controls audio className="rectangle" preload="metadata">
+        <source src="./plate.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
       </div>
     </div>
-  )
+    )
 }
 
 export default Videos
